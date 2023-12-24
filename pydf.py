6fd2fec13d192b5939b8f902384bf4fbd7f25430
d3ndr1t30x <117501042+d3ndr1t30x@users.noreply.github.com>
2023-12-24 17:48:09 +1000

    V2
    Tab to autocomplete added

--- pydf.py
+++ pydf.py
@@ -1,5 +1,7 @@
 import pdf2docx
 import docx2txt
+import argparse
+import argcomplete
 
 def convert_pdf_to_docx(pdf_file, docx_file):
     try:
@@ -20,39 +22,41 @@ def save_as_txt(docx_file):
 
 def print_banner():
     banner = r"""
-.----------------.  .----------------.  .----------------.  .----------------.   
-| .--------------. || .--------------. || .--------------. || .--------------. |  
-| |   ______     | || |  ____  ____  | || |  ________    | || |  _________   | |  
-| |  |_   __ \   | || | |_  _||_  _| | || | |_   ___ `.  | || | |_   ___  |  | |  
-| |    | |__) |  | || |   \ \  / /   | || |   | |   `. \ | || |   | |_  \_|  | |  
-| |    |  ___/   | || |    \ \/ /    | || |   | |    | | | || |   |  _|      | |  
-| |   _| |_      | || |    _|  |_    | || |  _| |___.' / | || |  _| |_       | |  
-| |  |_____|     | || |   |______|   | || | |________.'  | || | |_____|      | |  
-| |              | || |              | || |              | || |              | |  
-| '--------------' || '--------------' || '--------------' || '--------------' |  
- '----------------'  '----------------'  '----------------'  '----------------'   
-"""
+    .----------------.  .----------------.  .----------------.  .----------------.   
+    | .--------------. || .--------------. || .--------------. || .--------------. |  
+    | |   ______     | || |  ____  ____  | || |  ________    | || |  _________   | |  
+    | |  |_   __ \   | || | |_  _||_  _| | || | |_   ___ `.  | || | |_   ___  |  | |  
+    | |    | |__) |  | || |   \ \  / /   | || |   | |   `. \ | || |   | |_  \_|  | |  
+    | |    |  ___/   | || |    \ \/ /    | || |   | |    | | | || |   |  _|      | |  
+    | |   _| |_      | || |    _|  |_    | || |  _| |___.' / | || |  _| |_       | |  
+    | |  |_____|     | || |   |______|   | || | |________.'  | || | |_____|      | |  
+    | |              | || |              | || |              | || |              | |  
+    | '--------------' || '--------------' || '--------------' || '--------------' |  
+     '----------------'  '----------------'  '----------------'  '----------------'   
+    """
     print(banner)
 
 def main():
     print_banner()
     print("Welcome to PyDF - PDF to Word Converter")
-    
-    pdf_file = input("Enter the name of the PDF file (including extension): ").strip()
-    
-    if not pdf_file:
-        print("No PDF file specified. Exiting.")
-        return
 
-    docx_file = input("Enter the name for the output Word document (including extension, press Enter for default): ").strip()
+    parser = argparse.ArgumentParser(description="Convert PDF to Word document")
+    parser.add_argument("pdf_file", help="Name of the PDF file (including extension)")
+    parser.add_argument("--docx_file", help="Name for the output Word document (including extension, default is PDF filename with .docx extension)")
+    parser.add_argument("-c", "--copy_text", action="store_true", help="Copy text to a separate .txt file")
+
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+
+    pdf_file = args.pdf_file
+    docx_file = args.docx_file
 
     if not docx_file:
         docx_file = pdf_file.replace(".pdf", ".docx")
 
     convert_pdf_to_docx(pdf_file, docx_file)
 
-    copy_text_option = input("Copy text to a separate .txt file (y/n)? ").strip().lower()
-    if copy_text_option == "y":
+    if args.copy_text:
         save_as_txt(docx_file)
 
 if __name__ == "__main__":
